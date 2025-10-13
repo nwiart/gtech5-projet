8a0edfe2118bd3c5d0652728dcbb53cd27a138d2
nwiart <nwiart@gaming.tech>
2025-10-13 14:59:46 +0200

    Barebones dialogue system with UI

---
+++ Source/GT5_Projet/Public/Dialogue/DialogueCharacter.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DialogueCharacter.generated.h"
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct GT5_PROJET_API FDialogueCharacter
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FString Name;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	TMap<FString, UTexture2D*> Sprites;
+};
+
+
+USTRUCT(BlueprintType)
+struct GT5_PROJET_API FDialogueLine
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FString Text;
+};
