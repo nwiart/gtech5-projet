0d5bd3849ae28ea8e607c4a6a19d9ff76b94428f
nwiart <nwiart@gaming.tech>
2025-10-16 13:48:02 +0200

    Map element + reorganized my files

---
+++ Source/GT5_Projet/Private/MapElement.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MapElement.h"
+
+// Sets default values
+AMapElement::AMapElement()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>("SceneRoot");
+}
+
+// Called when the game starts or when spawned
+void AMapElement::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMapElement::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
