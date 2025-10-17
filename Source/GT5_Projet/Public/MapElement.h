69611204e6597a2271dadcc8eee263f58ef67352
WIART Noah <92325695+nwiart@users.noreply.github.com>
2025-10-17 09:13:09 +0200

    Base player, camera & map

---
+++ Source/GT5_Projet/Public/MapElement.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MapElement.generated.h"
+
+UCLASS()
+class GT5_PROJET_API AMapElement : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AMapElement();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	void GetTilePosition(int& outX, int& outY) const;
+
+	FIntPoint GetTilePosition() const;
+
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnPlayerHit();
+};
