14a5ffd214499522b636a291e7ceb4c17500c81b
francesca-attindehou <francesca.attindehou@epitech.eu>
2025-11-19 17:34:11 +0100

    Game Map generation

---
+++ Source/GT5_Projet/Private/MatchingTilesGame/GridWidget.cpp
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MatchingTilesGame/GridWidget.h"
+#include "Components/GridSlot.h"
+
+void UGridWidget::NativeConstruct()
+{
+    Super::NativeConstruct();
+
+    if (!Generator)
+    {
+        Generator = NewObject<UGridGeneratorComponent>(this);
+        Generator->MapDataTable = MapDataTable;
+        MapRowName = FName("Map0");
+    }
+
+    int32 Width = 0;
+    int32 Height = 0;
+    TArray<int32> Tiles;
+
+    Generator->LoadMap(MapRowName, Width, Height, Tiles);
+
+    if (Tiles.Num() == 0)
+    {
+        UE_LOG(LogTemp, Error, TEXT("Map is empty!"));
+        return;
+    }
+
+    BuildGrid(Width, Height, Tiles);
+}
+
+void UGridWidget::BuildGrid(int32 Width, int32 Height, const TArray<int32>& Tiles)
+{
+    if (!GridPanel)
+    {
+        UE_LOG(LogTemp, Error, TEXT("GridPanel not assigned!"));
+        return;
+    }
+
+    GridPanel->ClearChildren();
+
+    for (int y = 0; y < Height; ++y)
+    {
+        for (int x = 0; x < Width; ++x)
+        {
+            int index = y * Width + x;
+            int TileID = Tiles[index];
+
+            UTileWidget* Tile = CreateWidget<UTileWidget>(this, TileWidgetClass);
+            Tile->TileValue = Tiles[index];
+            FString RowNameStr = FString::Printf(TEXT("Tile_%d"), TileID);
+            FTileTypeData* TileData = TileTypeDataTable->FindRow<FTileTypeData>(FName(*RowNameStr), TEXT(""));
+            if (TileData && Tile->TileImage)
+                Tile->TileImage->SetBrushFromTexture(TileData->TileTexture);
+            UGridSlot* slot = GridPanel->AddChildToGrid(Tile, y, x);
+            slot->SetPadding(FMargin(2.f));
+        }
+    }
+
+    UE_LOG(LogTemp, Log, TEXT("Grid built successfully"));
+}
