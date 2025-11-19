582962f43a30bf3dce602d52a7eb48423ec93918
francesca-attindehou <francesca.attindehou@epitech.eu>
2025-11-19 18:46:06 +0100

    UPD tiles size

--- Source/GT5_Projet/Private/MatchingTilesGame/TileWidget.cpp
+++ Source/GT5_Projet/Private/MatchingTilesGame/TileWidget.cpp
@@ -1,6 +1,26 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Components/Button.h"
 #include "Components/Image.h"
+#include <Components/SizeBox.h>
+#include "Blueprint/WidgetTree.h"
 #include "MatchingTilesGame/TileWidget.h"
+
+void UTileWidget::NativeConstruct()
+{
+    Super::NativeConstruct();
+
+    if (!TileSizeBox)
+    {
+        TileSizeBox = WidgetTree->ConstructWidget<USizeBox>(USizeBox::StaticClass(), TEXT("TileSizeBox"));
+    }
+
+    if (!TileImage)
+    {
+        TileImage = WidgetTree->ConstructWidget<UImage>(UImage::StaticClass(), TEXT("TileImage"));
+        TileSizeBox->AddChild(TileImage);
+    }
+
+    if (!WidgetTree->RootWidget)
+        WidgetTree->RootWidget = TileSizeBox;
+}
