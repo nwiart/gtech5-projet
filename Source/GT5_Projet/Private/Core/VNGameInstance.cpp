c56e37f8e6b4d39216ce4f181a938f221ff48fda
nwiart <nwiart@gaming.tech>
2025-12-18 14:13:52 +0100

    Custom C++ game instance for global stuff

---
+++ Source/GT5_Projet/Private/Core/VNGameInstance.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Core/VNGameInstance.h"
+
+
+void UVNGameInstance::Init()
+{
+	Super::Init();
+
+}
+
+void UVNGameInstance::Shutdown()
+{
+	Super::Shutdown();
+
+}
