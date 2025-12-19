1b9105c18f8d84c8d9b12f1cf78e192402ef6593
nwiart <nwiart@gaming.tech>
2025-12-19 15:38:04 +0100

    Reorganized files a bit

--- Source/GT5_Projet/Private/Save/VNSaveManager.cpp
+++ Source/GT5_Projet/Private/Save/VNSaveManager.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Core/VNSaveManager.h"
+#include "Save/VNSaveManager.h"
 
-#include "Core/VNSaveGame.h"
+#include "Save/VNSaveGame.h"
 #include "Kismet/GameplayStatics.h"
 
 
