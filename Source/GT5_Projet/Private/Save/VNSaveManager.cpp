9cd354f90db04c8b83dc15a80eab1f34035ba71f
nwiart <nwiart@gaming.tech>
2026-01-13 12:02:56 +0100

    Implementing chapter stats in save game

--- Source/GT5_Projet/Private/Save/VNSaveManager.cpp
+++ Source/GT5_Projet/Private/Save/VNSaveManager.cpp
@@ -12,17 +12,27 @@ UVNSaveManager::UVNSaveManager()
 
 }
 
-void UVNSaveManager::Load()
+void UVNSaveManager::Load(TSubclassOf<UVNSaveGame> saveGameClass)
 {
 	SaveGame = (UVNSaveGame*) UGameplayStatics::LoadGameFromSlot("Slot", 0);
 	if (SaveGame == NULL) {
-		SaveGame = NewObject<UVNSaveGame>(this);
+		SaveGame = NewObject<UVNSaveGame>(this, saveGameClass);
 	}
 }
 
 void UVNSaveManager::Save()
 {
+	if (SaveGame == NULL) {
+		UE_LOG(LogTemp, Warning, TEXT("SaveGame was null! Did you forget to Load() the save?"));
+		return;
+	}
+
 	UGameplayStatics::SaveGameToSlot(SaveGame, "Slot", 0);
+}
+
+const UClass* UVNSaveManager::GetSaveGameClass() const
+{
+	return SaveGame ? SaveGame->GetClass() : 0;
 }
 
 FTableEvents UVNSaveManager::GetEventData(const FGuid& Guid)
@@ -38,5 +48,9 @@ FTableEvents UVNSaveManager::GetEventData(const FGuid& Guid)
 
 void UVNSaveManager::SetEventData(const FGuid& Guid, const FTableEvents& Data)
 {
+	if (SaveGame == NULL) {
+		return;
+	}
+
 	SaveGame->Events.Add(Guid, Data);
 }
