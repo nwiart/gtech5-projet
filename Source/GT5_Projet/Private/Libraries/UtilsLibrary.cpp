d5444750ae5a7afd9b2cef2ce16865cff31f47d0
Vy Vu <74190308+bellavee@users.noreply.github.com>
2026-03-05 14:13:57 +0100

    add true first chapter

--- Source/GT5_Projet/Private/Libraries/UtilsLibrary.cpp
+++ Source/GT5_Projet/Private/Libraries/UtilsLibrary.cpp
@@ -5,9 +5,20 @@
 #include "Kismet/GameplayStatics.h"
 
 
-void UUtilsLibrary::GetActorsOfClassInStreamedLevel(TArray<AActor*>& OutActors, ULevelStreaming* LevelStreaming, const UClass* Class, const UObject* WorldContextObject)
+bool UUtilsLibrary::GetActorsOfClassInStreamedLevel(TArray<AActor*>& OutActors, ULevelStreaming* LevelStreaming, const UClass* Class, const UObject* WorldContextObject)
 {
+	if (!LevelStreaming || !Class) {
+		UE_LOG(LogTemp, Warning, TEXT("UUtilsLibrary::GetActorsOfClassInStreamedLevel() : LevelStreaming and Class must be non-null!"));
+		return false;
+	}
+
+	if (!LevelStreaming->GetLoadedLevel()) {
+		UE_LOG(LogTemp, Warning, TEXT("UUtilsLibrary::GetActorsOfClassInStreamedLevel() : LevelStreaming reports level not being loaded."));
+		return false;
+	}
+
 	OutActors = LevelStreaming->GetLoadedLevel()->Actors.FilterByPredicate([Class](const AActor* actor) {
 		return actor && actor->GetClass()->IsChildOf(Class);
 	});
+	return true;
 }
